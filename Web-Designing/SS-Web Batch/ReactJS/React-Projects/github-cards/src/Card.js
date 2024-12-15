2ea3f3341b2c0b80d3c981e8fb8c813843681205
Sneha Lohana <sneha.lohana16@gmail.com>
2024-12-15 08:57:56 +0530

    Day3-ReactJS-14th Dec

--- Web-Designing/SS-Web Batch/ReactJS/React-Projects/github-cards/src/Card.js
+++ Web-Designing/SS-Web Batch/ReactJS/React-Projects/github-cards/src/Card.js
@@ -1,5 +1,18 @@
-export default function Card(){
+import './Card.css';
+
+export default function Card(props){
+    // const profile = props.profile;
+    const profile = props;
+    // console.log(props);
+    
     return (
-        <div>Card</div>
+        <div className="profile-info">
+            {/* Inline CSS */}
+            <img src={profile.avatar_url} alt={profile.login} style={{width:'150px',borderRadius: "50%"}}/>
+            <div>
+                <div>{profile.login}</div>
+                <a href={profile.html_url} target="_blank" rel="noreferrer">Github Link</a>
+            </div>
+        </div>
     )
 }
