fd970d53b0773d88d5e2db019f6893d7f310a219
Sneha Lohana <sneha.lohana16@gmail.com>
2024-12-17 13:03:00 +0530

    ReactJS - Day 5 - 17th Dec

---
+++ Web-Designing/SS-Web Batch/ReactJS/React-Projects/my-shop/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        My-Shop
+      </header>
+    </div>
+  );
+}
+
+export default App;
